d74c3e5dc395a144500d219faf3610ef2697e052
morakinyo75 <akinwandeemmanuel899@gmail.com>
2023-06-29 13:41:55 +0000

    0x02-ES6_classes

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,7 +1,14 @@
-import EVCar from './100-evcar.js';
+import Car from './10-car';
 
-const ec1 = new EVCar("Tesla", "Turbo", "Red", "250");
-console.log(ec1);
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
 
-const ec2 = ec1.cloneCar();
-console.log(ec2);
+   cloneCar() {
+    const NewObj = this.constructor[Symbol.species] || this.constructor;
+    const clone = new NewObj();
+    return clone;
+   }
+}
